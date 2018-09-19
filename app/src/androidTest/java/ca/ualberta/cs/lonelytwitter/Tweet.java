6cc54695bc85d954b95cef50a92e2240b6e03f61
Jonathan David Armfield <jarmfiel@ualberta.ca>
2018-09-18 19:10:27 -0600

    Create Tweet.java

---
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -0,0 +1,32 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+public abstract class Tweet implements Tweetable {
+    private Date date;
+    private String message;
+    private static final Integer MAX_CHARS = 140;
+
+    Tweet(){
+        this.date = new Date();
+        this.message = "default message: destroy all humans";
+
+    }
+    Tweet(String message)   {
+        this.date = new Date();
+        this.message = message;
+
+
+    }
+    public Date getDate()   {return this.date;}
+    public String getMessage()  {return this.message}
+    public void setMessage(String message)  {
+        if (message.length() <= MAX_CHARS)  {
+            this.message = message;
+        } else{
+            throw new TweetTooLongException();
+        }
+
+    }
+    public abstract Booleab isImportant();
+}
